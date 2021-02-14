f8b0b13dcd3d2fd0035598c19bfef06d67b0760d
AditiShubhsri404 <73790417+AditiShubhsri@users.noreply.github.com>
2021-02-14 07:51:18 +0530

    Add files via upload

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBBoSnYAnDvYqKcZiFzPK9_XUy3wzJS9vk",
+    authDomain: "mygitapp-187fc.firebaseapp.com",
+    projectId: "mygitapp-187fc",
+    storageBucket: "mygitapp-187fc.appspot.com",
+    messagingSenderId: "717342426241",
+    appId: "1:717342426241:web:96249d813b7a9860143980",
+    measurementId: "G-6BC1YD7SDH"
+  };
